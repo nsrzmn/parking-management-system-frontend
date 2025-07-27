cfe1df3e6daa6b8127b2b26abd3740023bb50cca
Nasir Zaman <58137523+nsrzmn@users.noreply.github.com>
2025-07-27 20:07:00 +0200

    Update vite.config.ts
    deployment update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/parking-management-system-frontend/', // 👈 Add this line
   plugins: [react()],
 })
